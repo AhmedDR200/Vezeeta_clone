ebb4dccc9d99e8fde87be93fa08d1224475f7657
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-29 00:07:05 +0300

    templates setup3 'static files'

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls', namespace='accounts'))
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
