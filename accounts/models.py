3bba5ece6eb205e84e8691f160a1c1a7bd36bed3
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-29 00:25:19 +0300

    templates setup4

--- accounts/models.py
+++ accounts/models.py
@@ -9,13 +9,13 @@ class Profile(models.Model):
   name = models.CharField(_("الاسم :"), max_length=50)
   who_i = models.TextField(_("من انا :"), max_length=250)  
   price = models.IntegerField(_("سعر الكشف :"))
-  image = models.ImageField(_("الصورة الشخصية"), upload_to='profile')
+  
 
   class Meta:
         verbose_name = _("Profile")
         verbose_name_plural = _("Profiles")
 
   def __str__(self):
-        return '%s' %(self.user.username)
+        return self.name
   
 
