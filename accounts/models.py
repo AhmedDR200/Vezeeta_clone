0121ec2af2f5780ab7e08637f2c805111f49f762
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-29 13:41:49 +0300

    edit models file 'django signals'

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
+from django.db.models.signals import post_save
 # Create your models here.
 
 
@@ -8,7 +9,7 @@ class Profile(models.Model):
   user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
   name = models.CharField(_("الاسم :"), max_length=50)
   who_i = models.TextField(_("من انا :"), max_length=250)  
-  price = models.IntegerField(_("سعر الكشف :"))
+  price = models.IntegerField(_("سعر الكشف :"), null=True, blank=True)
   
 
   class Meta:
@@ -16,6 +17,12 @@ class Profile(models.Model):
         verbose_name_plural = _("Profiles")
 
   def __str__(self):
-        return self.name
+        return '%s' %(self.user.username)
   
 
+def create_profile(sender, **kwargs):
+     if kwargs['created']:
+          Profile.objects.create(user= kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
