1ae7ee3cd1ee92b08214d7a2dd4edea98613232a
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-30 14:42:08 +0300

    myprofile template2

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,21 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class UserCreationForms(UserCreationForm):
+    username = forms.CharField(label='الاسم')
+    firstname = forms.CharField(label='الاسم الأول')
+    lastname = forms.CharField(label='الاسم الأخير')
+    email = forms.EmailField(label='البريد الالكتروني')
+    password1 = forms.EmailField(label='كلمة المرور', widget=forms.PasswordInput(), min_length=8)
+    password2 = forms.EmailField(label='تأكيد كلمة المرور', widget=forms.PasswordInput(), min_length=8)
+
+    class Meta:
+        model = User
+        fields = ('username','firstname','lastname','email','password1','password2')
+                                       
 
 
 
